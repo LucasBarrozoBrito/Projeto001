2796ac0ea323bc46c8e07ba251536eeead166ece
Lucas Brito <lucas2022mkt@gmail.com>
2023-03-22 20:11:11 -0300

    00

---
+++ modelo.js
@@ -0,0 +1,44 @@
+function contar(){
+    var ini = document.getElementById ('ini1')
+    var fim = document.getElementById ('fi3')
+    var passo = document.getElementById ('mei2')
+    var res = document.getElementById ('res')
+
+
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        res.innerHTML = `Impossível contar! `
+
+        
+    } else {
+        res.innerHTML = `Contando: `
+        var i = Number (ini.value)
+        var f = Number (fim.value)
+        var p = Number (passo.value)
+
+        if(p <= 0) {
+            window.alert('Passo invalido! Considerando Passo 1')
+            p = 1
+        }
+        if(i < f){
+            //Contagem crescente
+            for (var c = i; c <= f; c += p){
+                res.innerHTML += ` ${c} \u{1F449}`
+                
+            }
+        }else{
+            //Contagem regressiva
+            for (var c = i; c >= f; c -= p){
+                res.innerHTML += ` ${c} \u{1F449} `
+                
+            }
+        }
+
+        res.innerHTML += `\u{1F3C1}` 
+
+        
+    }
+
+
+
+}
+
